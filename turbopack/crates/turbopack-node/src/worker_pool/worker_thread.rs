eaf47da0afd205af7079a2e2fcd178ac4f928cdc
xusd320 <xusd320@gmail.com>
2025-11-26 02:30:37 +0800

    fix: features conflict when cargo clippy

--- turbopack/crates/turbopack-node/src/worker_pool/worker_thread.rs
+++ turbopack/crates/turbopack-node/src/worker_pool/worker_thread.rs
@@ -3,12 +3,14 @@ use napi_derive::napi;
 use crate::worker_pool::operation::WORKER_POOL_OPERATION;
 
 #[napi(object)]
+#[allow(unused)]
 pub struct PoolOptions {
     pub filename: String,
     pub concurrency: u32,
 }
 
 #[napi]
+#[allow(unused)]
 pub fn recv_pool_creation() -> Option<PoolOptions> {
     WORKER_POOL_OPERATION
         .try_recv_pool_creation()
@@ -19,6 +21,7 @@ pub fn recv_pool_creation() -> Option<PoolOptions> {
 }
 
 #[napi]
+#[allow(unused)]
 pub async fn recv_worker_request(pool_id: String) -> napi::Result<String> {
     WORKER_POOL_OPERATION
         .recv_worker_request(pool_id)
@@ -28,6 +31,7 @@ pub async fn recv_worker_request(pool_id: String) -> napi::Result<String> {
 
 #[napi]
 // TODO: use zero-copy externaled type array
+#[allow(unused)]
 pub async fn recv_message_in_worker(worker_id: u32) -> napi::Result<String> {
     Ok(WORKER_POOL_OPERATION
         .recv_message_in_worker(worker_id)
@@ -35,6 +39,7 @@ pub async fn recv_message_in_worker(worker_id: u32) -> napi::Result<String> {
 }
 
 #[napi]
+#[allow(unused)]
 pub async fn notify_one_worker_created(filename: String) -> napi::Result<()> {
     WORKER_POOL_OPERATION
         .notify_one_worker_created(filename)
@@ -43,6 +48,7 @@ pub async fn notify_one_worker_created(filename: String) -> napi::Result<()> {
 }
 
 #[napi]
+#[allow(unused)]
 pub async fn notify_worker_ack(task_id: String, worker_id: u32) -> napi::Result<()> {
     WORKER_POOL_OPERATION
         .notify_worker_ack(task_id, worker_id)
@@ -51,6 +57,7 @@ pub async fn notify_worker_ack(task_id: String, worker_id: u32) -> napi::Result<
 }
 
 #[napi]
+#[allow(unused)]
 pub async fn send_task_message(task_id: String, message: String) -> napi::Result<()> {
     WORKER_POOL_OPERATION
         .send_task_message(task_id, message)
