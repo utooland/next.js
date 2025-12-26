da96ced090e9e4b2cf86e9b149710c128d8eee93
zoomdong <1344492820@qq.com>
2025-12-25 22:26:29 -0800

    chore: remove dumplicate clean_separators (#86)

--- turbopack/crates/turbopack-core/src/ident.rs
+++ turbopack/crates/turbopack-core/src/ident.rs
@@ -433,7 +433,7 @@ impl ValueToString for AssetIdent {
     }
 }
 
-fn clean_separators(s: &str) -> String {
+pub fn clean_separators(s: &str) -> String {
     static SEPARATOR_REGEX: Lazy<Regex> = Lazy::new(|| Regex::new(r"[/#?\[\]<>@]").unwrap());
     SEPARATOR_REGEX.replace_all(s, "_").to_string()
 }
