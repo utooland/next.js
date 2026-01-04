1f13d5fbcb0b822f87fbb6f439f2d525d3255c16
xusd320 <xusd320@gmail.com>
2026-01-04 18:41:04 +0800

    Revert "Add dependency stress benchmark"
    This reverts commit 5c3067b9c5059e893140f50cbb7a9fd3889b6644.

--- turbopack/crates/turbo-tasks-backend/benches/mod.rs
+++ turbopack/crates/turbo-tasks-backend/benches/mod.rs
@@ -3,7 +3,6 @@
 
 use criterion::{Criterion, criterion_group, criterion_main};
 
-pub(crate) mod dependency_stress;
 pub(crate) mod overhead;
 pub(crate) mod scope_stress;
 pub(crate) mod stress;
@@ -11,6 +10,6 @@ pub(crate) mod stress;
 criterion_group!(
     name = turbo_tasks_backend_stress;
     config = Criterion::default();
-    targets = stress::fibonacci, scope_stress::scope_stress, overhead::overhead, dependency_stress::dependency_stress
+    targets = stress::fibonacci, scope_stress::scope_stress, overhead::overhead
 );
 criterion_main!(turbo_tasks_backend_stress);
