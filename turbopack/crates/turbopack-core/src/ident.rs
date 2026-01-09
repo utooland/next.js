312ba1580d676c7ab9b5ca072a7b0a3b2e85804a
zoomdong <1344492820@qq.com>
2026-01-09 10:57:12 +0800

    fix(turbopack): add () and space to invalid seperator (#94)

--- turbopack/crates/turbopack-core/src/ident.rs
+++ turbopack/crates/turbopack-core/src/ident.rs
@@ -434,7 +434,7 @@ impl ValueToString for AssetIdent {
 }
 
 pub fn clean_separators(s: &str) -> String {
-    static SEPARATOR_REGEX: Lazy<Regex> = Lazy::new(|| Regex::new(r"[/#?\[\]<>@]").unwrap());
+    static SEPARATOR_REGEX: Lazy<Regex> = Lazy::new(|| Regex::new(r"[/#?\[\]<>@\s()]").unwrap());
     SEPARATOR_REGEX.replace_all(s, "_").to_string()
 }
 
