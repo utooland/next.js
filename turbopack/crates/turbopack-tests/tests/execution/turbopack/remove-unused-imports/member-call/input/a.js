utooland/next.js

commit cc0575745a4bf057659503837cedf59ce54ee452

Turbopack: move import_usage computation into ImportMap (#92891)
I'm moving all of the export-related logic into `EvalContext` (and thus
`ImportMap`) so that `VarGraph` is only needed for computing the
references, and not the exports.

turbopack/crates/turbopack-ecmascript/src/analyzer/imports.rs
@@ -11,7 +11,7 @@ use rustc_hash::{FxHashMap, FxHashSet};
 use smallvec::SmallVec;
 use swc_core::{
     atoms::Wtf8Atom,
-    common::{BytePos, Span, Spanned, SyntaxContext, comments::Comments},
+    common::{BytePos, Mark, Span, Spanned, SyntaxContext, comments::Comments},
     ecma::{
         ast::*,
         atoms::{Atom, atom},
@@ -22,7 +22,7 @@ use swc_core::{
 use turbo_frozenmap::FrozenMap;
 use turbo_rcstr::{RcStr, rcstr};
 use turbo_tasks::{FxIndexMap, FxIndexSet, ResolvedVc};
-use turbopack_core::loader::WebpackLoaderItem;
+use turbopack_core::{loader::WebpackLoaderItem, resolve::ImportUsage};
 
 use super::{JsValue, ModuleValue, top_level_await::has_top_level_await};
 use crate::{
@@ -30,6 +30,7 @@ use crate::{
     analyzer::{
         ConstantValue, ObjectPart,
         graph::{AssignmentScope, AssignmentScopes, EvalContext},
+        is_unresolved,
     },
     magic_identifier::{MAGIC_IDENTIFIER_DEFAULT_EXPORT, MAGIC_IDENTIFIER_DEFAULT_EXPORT_ATOM},
     references::{
@@ -254,6 +255,88 @@ impl Display for ImportAnnotations {
     }
 }
 
+#[derive(Clone, Debug)]
+pub enum DeclUsage {
+    SideEffects,
+    Bindings(FxHashSet<Id>),
+}
+impl Default for DeclUsage {
+    fn default() -> Self {
+        DeclUsage::Bindings(Default::default())
+    }
+}
+impl DeclUsage {
+    fn add_usage(&mut self, user: &Id) {
+        match self {
+            Self::Bindings(set) => {
+                set.insert(user.clone());
+            }
+            Self::SideEffects => {}
+        }
+    }
+    fn make_side_effects(&mut self) {
+        *self = Self::SideEffects;
+    }
+}
+
+#[derive(Default, Debug)]
+pub(crate) struct ProgramDeclUsage {
+    // ident -> immediate usage (top level decl)
+    pub(crate) decl_usages: FxHashMap<Id, DeclUsage>,
+    // import -> immediate usage (top level decl)
+    pub(crate) import_usages: FxHashMap<usize, DeclUsage>,
+    // export name -> top level decl
+    pub(crate) exports: FxHashMap<RcStr, Id>,
+}
+impl ProgramDeclUsage {
+    fn compute_import_usage(&self) -> FxHashMap<usize, ImportUsage> {
+        let mut import_usage =
+            FxHashMap::with_capacity_and_hasher(self.import_usages.len(), Default::default());
+        for (reference, usage) in &self.import_usages {
+            // TODO make this more efficient, i.e. cache the result?
+            if let DeclUsage::Bindings(ids) = usage {
+                // compute transitive closure of `ids` over `top_level_mappings`
+                let mut visited = ids.clone();
+                let mut stack = ids.iter().collect::<Vec<_>>();
+                let mut has_global_usage = false;
+                while let Some(id) = stack.pop() {
+                    match self.decl_usages.get(id) {
+                        Some(DeclUsage::SideEffects) => {
+                            has_global_usage = true;
+                            break;
+                        }
+                        Some(DeclUsage::Bindings(callers)) => {
+                            for caller in callers {
+                                if visited.insert(caller.clone()) {
+                                    stack.push(caller);
+                                }
+                            }
+                        }
+                        _ => {}
+                    }
+                }
+
+                // Collect all `visited` declarations which are exported
+                import_usage.insert(
+                    *reference,
+                    if has_global_usage {
+                        ImportUsage::TopLevel
+                    } else {
+                        ImportUsage::Exports(
+                            self.exports
+                                .iter()
+                                .filter(|(_, id)| visited.contains(*id))
+                                .map(|(exported, _)| exported.clone())
+                                .collect(),
+                        )
+                    },
+                );
+            }
+        }
+        import_usage
+    }
+}
+
 /// A version of [crate::references::esm::export::EsmExport] with usize instead of the module
 /// reference Vc, and missing the liveness fields.
 #[derive(Debug)]
@@ -322,6 +405,8 @@ pub(crate) struct ImportMap {
     /// Map from export binding id to the scopes where it's assigned. This is used to determine
     /// whether an export is live or not.
     pub(super) assignment_scopes: FxHashMap<Id, AssignmentScopes>,
+
+    pub(crate) import_usage: FxHashMap<usize, ImportUsage>,
 
     /// Map from exported name to local binding id (includes the syntax context).
     pub(crate) exports_ids: FxHashMap<RcStr, Id>,
@@ -465,10 +550,9 @@ impl ImportMap {
         self.attributes.get(&span.lo).unwrap_or_default()
     }
 
-    // TODO this could return &str instead of String to avoid cloning
-    pub fn get_binding(&self, id: &Id) -> Option<(usize, Option<RcStr>)> {
+    pub fn get_binding(&self, id: &Id) -> Option<(usize, Option<&Atom>)> {
         if let Some((i, i_sym)) = self.imports.get(id) {
-            return Some((*i, Some(i_sym.as_str().into())));
+            return Some((*i, Some(i_sym)));
         }
         if let Some(i) = self.namespace_imports.get(id) {
             return Some((*i, None));
@@ -555,13 +639,19 @@ impl ImportMap {
     }
 
     /// Analyze ES import
-    pub(super) fn analyze(m: &Program, comments: Option<&dyn Comments>) -> Self {
+    pub(super) fn analyze(
+        unresolved_mark: Mark,
+        m: &Program,
+        comments: Option<&dyn Comments>,
+    ) -> Self {
         let mut data = ImportMap::default();
         let mut analyzer = Analyzer {
+            unresolved_mark,
             data: &mut data,
             comments,
             namespace_imports_to_specifier: FxIndexMap::default(),
-            is_in_fn: false,
+            state: Default::default(),
+            program_decl_usage: Default::default(),
         };
 
         // A prepass to detect imports to be able to rewrite import+export pairs to true reexports
@@ -665,6 +755,8 @@ impl ImportMap {
 
         m.visit_with(&mut analyzer);
 
+        data.import_usage = analyzer.program_decl_usage.compute_import_usage();
+
         data
     }
 
@@ -675,14 +767,69 @@ impl ImportMap {
     }
 }
 
+mod analyzer_state {
+    use swc_core::ecma::ast::{Id, Ident};
+
+    use super::Analyzer;
+
+    #[derive(Default)]
+    pub(super) struct AnalyzerState {
+        is_in_fn: bool,
+        cur_top_level_decl_name: Option<Id>,
+    }
+
+    impl AnalyzerState {
+        /// Returns the identifier of the current top level declaration.
+        pub(super) fn cur_top_level_decl_name(&self) -> &Option<Id> {
+            &self.cur_top_level_decl_name
+        }
+
+        /// Returns whether the current context is inside a function.
+        pub(super) fn is_in_fn(&self) -> bool {
+            self.is_in_fn
+        }
+    }
+
+    impl Analyzer<'_> {
+        /// Runs `visitor` with the current top level declaration identifier
+        pub(super) fn enter_top_level_decl<T>(
+            &mut self,
+            name: &Ident,
+            visitor: impl FnOnce(&mut Self) -> T,
+        ) -> T {
+            let is_top_level_fn = self.state.cur_top_level_decl_name.is_none();
+            if is_top_level_fn {
+                self.state.cur_top_level_decl_name = Some(name.to_id());
+            }
+            let result = visitor(self);
+            if is_top_level_fn {
+                self.state.cur_top_level_decl_name = None;
+            }
+            result
+        }
+
+        /// Runs `visitor` with the right is_in_fn value
+        pub(super) fn enter_fn<T>(&mut self, visitor: impl FnOnce(&mut Self) -> T) -> T {
+            let old_is_in_fn = self.state.is_in_fn;
+            self.state.is_in_fn = true;
+            let result = visitor(self);
+            self.state.is_in_fn = old_is_in_fn;
+            result
+        }
+    }
+}
+
 struct Analyzer<'a> {
+    unresolved_mark: Mark,
     data: &'a mut ImportMap,
     comments: Option<&'a dyn Comments>,
     /// Map from local identifier of namespace imports to module path, used temporarily during
     /// analysis to detect dynamic accesses to namespace imports.
     namespace_imports_to_specifier: FxIndexMap<Id, Wtf8Atom>,
 
-    is_in_fn: bool,
+    program_decl_usage: ProgramDeclUsage,
+
+    state: analyzer_state::AnalyzerState,
 }
 
 impl Analyzer<'_> {
@@ -709,7 +856,7 @@ impl Analyzer<'_> {
     }
 
     fn register_assignment_scope(&mut self, id: Id) {
-        let scope = if self.is_in_fn {
+        let scope = if self.state.is_in_fn() {
             AssignmentScope::Function
         } else {
             AssignmentScope::ModuleEval
@@ -727,6 +874,10 @@ impl Analyzer<'_> {
 }
 
 impl Visit for Analyzer<'_> {
+    fn visit_import_decl(&mut self, _: &ImportDecl) {
+        // We already handled import above. Skip as the Idents in here confuse the analysis
+    }
+
     fn visit_export_all(&mut self, export: &ExportAll) {
         if export.type_only {
             return;
@@ -743,6 +894,7 @@ impl Visit for Analyzer<'_> {
         );
         self.data.reexport_namespaces.push(i);
         self.data.has_exports = true;
+        export.visit_children_with(self);
     }
 
     fn visit_named_export(&mut self, export: &NamedExport) {
@@ -830,7 +982,11 @@ impl Visit for Analyzer<'_> {
                                 // This is a export of an imported binding. Rewrite to a true
                                 // reexport.
                                 if let Some(export) = export {
-                                    Export::ImportedBinding(index, export, is_fake_esm)
+                                    Export::ImportedBinding(
+                                        index,
+                                        RcStr::from(export.as_str()),
+                                        is_fake_esm,
+                                    )
                                 } else {
                                     Export::ImportedNamespace(index)
                                 }
@@ -850,23 +1006,27 @@ impl Visit for Analyzer<'_> {
     }
 
     fn visit_export_decl(&mut self, n: &ExportDecl) {
-        n.visit_children_with(self);
         self.data.has_exports = true;
-
         match &n.decl {
             Decl::Class(n) => {
                 let name = RcStr::from(n.ident.sym.as_str());
                 self.data
                     .exports
                     .insert(name.clone(), Export::LocalBinding(name.clone(), false));
-                self.data.exports_ids.insert(name, n.ident.to_id());
+                self.data.exports_ids.insert(name.clone(), n.ident.to_id());
+                self.program_decl_usage
+                    .exports
+                    .insert(name, n.ident.to_id());
             }
             Decl::Fn(n) => {
                 let name = RcStr::from(n.ident.sym.as_str());
                 self.data
                     .exports
                     .insert(name.clone(), Export::LocalBinding(name.clone(), false));
-                self.data.exports_ids.insert(name, n.ident.to_id());
+                self.data.exports_ids.insert(name.clone(), n.ident.to_id());
+                self.program_decl_usage
+                    .exports
+                    .insert(name, n.ident.to_id());
             }
             Decl::Var(..) => {
                 let ids: Vec<Id> = find_pat_ids(&n.decl);
@@ -875,7 +1035,8 @@ impl Visit for Analyzer<'_> {
                     self.data
                         .exports
                         .insert(name.clone(), Export::LocalBinding(name.clone(), false));
-                    self.data.exports_ids.insert(name, id);
+                    self.data.exports_ids.insert(name.clone(), id.clone());
+                    self.program_decl_usage.exports.insert(name, id);
                 }
             }
             Decl::Using(_) => {
@@ -886,10 +1047,11 @@ impl Visit for Analyzer<'_> {
                 // ignore typescript for code generation
             }
         }
+
+        n.visit_children_with(self);
     }
 
     fn visit_export_default_decl(&mut self, n: &ExportDefaultDecl) {
-        n.visit_children_with(self);
         self.data.has_exports = true;
 
         let id = match &n.decl {
@@ -919,11 +1081,14 @@ impl Visit for Analyzer<'_> {
             rcstr!("default"),
             Export::LocalBinding(RcStr::from(id.0.as_str()), false),
         );
-        self.data.exports_ids.insert(rcstr!("default"), id);
+        self.data.exports_ids.insert(rcstr!("default"), id.clone());
+        self.program_decl_usage
+            .exports
+            .insert(rcstr!("default"), id);
+        n.visit_children_with(self);
     }
 
     fn visit_export_default_expr(&mut self, n: &ExportDefaultExpr) {
-        n.visit_children_with(self);
         self.data.has_exports = true;
 
         self.data.exports.insert(
@@ -938,21 +1103,32 @@ impl Visit for Analyzer<'_> {
                 SyntaxContext::empty(),
             ),
         );
+        n.visit_children_with(self);
     }
 
     fn visit_export_named_specifier(&mut self, n: &ExportNamedSpecifier) {
-        if let ModuleExportName::Ident(local) = &n.orig {
-            let exported = n.exported.as_ref().unwrap_or(&n.orig);
-            self.data
-                .exports_ids
-                .insert(exported.atom().as_str().into(), local.to_id());
-        }
+        self.data.has_exports = true;
+
+        let ModuleExportName::Ident(local) = &n.orig else {
+            unreachable!("exporting a string should be impossible")
+        };
+        let exported = RcStr::from(n.exported.as_ref().unwrap_or(&n.orig).atom().as_str());
+        self.data
+            .exports_ids
+            .insert(exported.clone(), local.to_id());
+        self.program_decl_usage
+            .exports
+            .insert(exported, local.to_id());
+        n.visit_children_with(self);
     }
 
     fn visit_export_default_specifier(&mut self, n: &ExportDefaultSpecifier) {
+        self.data.has_exports = true;
+
         self.data
             .exports_ids
             .insert(rcstr!("default"), n.exported.to_id());
+        n.visit_children_with(self);
     }
 
     fn visit_program(&mut self, m: &Program) {
@@ -1022,42 +1198,42 @@ impl Visit for Analyzer<'_> {
     }
 
     fn visit_getter_prop(&mut self, node: &GetterProp) {
-        let old_is_in_fn = self.is_in_fn;
-        self.is_in_fn = true;
-        node.visit_children_with(self);
-        self.is_in_fn = old_is_in_fn;
+        self.enter_fn(|this| {
+            node.visit_children_with(this);
+        });
     }
     fn visit_setter_prop(&mut self, node: &SetterProp) {
-        let old_is_in_fn = self.is_in_fn;
-        self.is_in_fn = true;
-        node.visit_children_with(self);
-        self.is_in_fn = old_is_in_fn;
+        self.enter_fn(|this| {
+            node.visit_children_with(this);
+        });
     }
     fn visit_function(&mut self, node: &Function) {
-        let old_is_in_fn = self.is_in_fn;
-        self.is_in_fn = true;
-        node.visit_children_with(self);
-        self.is_in_fn = old_is_in_fn;
+        self.enter_fn(|this| {
+            node.visit_children_with(this);
+        });
     }
     fn visit_constructor(&mut self, node: &Constructor) {
-        let old_is_in_fn = self.is_in_fn;
-        self.is_in_fn = true;
-        node.visit_children_with(self);
-        self.is_in_fn = old_is_in_fn;
+        self.enter_fn(|this| {
+            node.visit_children_with(this);
+        });
     }
     fn visit_arrow_expr(&mut self, node: &ArrowExpr) {
-        let old_is_in_fn = self.is_in_fn;
-        self.is_in_fn = true;
-        node.visit_children_with(self);
-        self.is_in_fn = old_is_in_fn;
+        self.enter_fn(|this| {
+            node.visit_children_with(this);
+        });
     }
 
     fn visit_member_expr(&mut self, node: &MemberExpr) {
         if let MemberProp::Ident(..) | MemberProp::PrivateName(..) = &node.prop
             && node.obj.is_ident()
         {
-            // Skip if obj is a Expr::Ident, so that it doesn't get added to full_star_imports below
-            // in visit_expr.
+            // Skip traversing if obj is a Expr::Ident, so that it doesn't get added to
+            // full_star_imports below in visit_expr.
+
+            // TODO this currently doesn't properly mark the import in self.program_decl_usage, see
+            // todo in
+            // turbopack/crates/turbopack-tests/tests/execution/turbopack/remove-unused-imports/
+            // import-star/input/index.js
             return;
         }
         node.visit_children_with(self);
@@ -1069,9 +1245,8 @@ impl Visit for Analyzer<'_> {
             if let Some(module_path) = self.namespace_imports_to_specifier.get(&i.to_id()) {
                 self.data.full_star_imports.insert(module_path.clone());
             }
-        } else {
-            pat.visit_children_with(self);
         }
+        pat.visit_children_with(self);
     }
 
     fn visit_simple_assign_target(&mut self, node: &SimpleAssignTarget) {
@@ -1080,18 +1255,52 @@ impl Visit for Analyzer<'_> {
             if let Some(module_path) = self.namespace_imports_to_specifier.get(&i.to_id()) {
                 self.data.full_star_imports.insert(module_path.clone());
             }
-        } else {
-            node.visit_children_with(self);
         }
+        node.visit_children_with(self);
     }
 
     fn visit_expr(&mut self, node: &Expr) {
-        if let Expr::Ident(i) = node {
-            if let Some(module_path) = self.namespace_imports_to_specifier.get(&i.to_id()) {
-                self.data.full_star_imports.insert(module_path.clone());
+        if let Expr::Ident(i) = node
+            && let Some(module_path) = self.namespace_imports_to_specifier.get(&i.to_id())
+        {
+            self.data.full_star_imports.insert(module_path.clone());
+        }
+        node.visit_children_with(self);
+    }
+
+    fn visit_ident(&mut self, node: &Ident) {
+        let id = node.to_id();
+        if let Some((esm_reference_index, _)) = self.data.get_binding(&id) {
+            // An import binding
+            let usage = self
+                .program_decl_usage
+                .import_usages
+                .entry(esm_reference_index)
+                .or_default();
+            if let Some(top_level) = self.state.cur_top_level_decl_name() {
+                usage.add_usage(top_level);
+            } else {
+                usage.make_side_effects();
             }
         } else {
-            node.visit_children_with(self);
+            // A regular variable
+            if !is_unresolved(node, self.unresolved_mark) {
+                if let Some(top_level) = self.state.cur_top_level_decl_name() {
+                    if &id != top_level {
+                        self.program_decl_usage
+                            .decl_usages
+                            .entry(id)
+                            .or_default()
+                            .add_usage(top_level);
+                    }
+                } else {
+                    self.program_decl_usage
+                        .decl_usages
+                        .entry(id)
+                        .or_default()
+                        .make_side_effects();
+                }
+            }
         }
     }
 
@@ -1100,6 +1309,12 @@ impl Visit for Analyzer<'_> {
             self.register_assignment_scope(ident.to_id());
         }
         node.visit_children_with(self);
+    }
+
+    fn visit_fn_decl(&mut self, node: &FnDecl) {
+        self.enter_top_level_decl(&node.ident, |this| {
+            node.visit_children_with(this);
+        });
     }
 
     fn visit_decl(&mut self, node: &Decl) {

commit 4976adbbf4196db3913f9933ffeb92ad2f17e612

Turbopack: fix import_usage for functions behind member expressions (#93504)
Manifested as `ReferenceError: foo is not defined` where `foo` is an
import

A regression from https://github.com/vercel/next.js/pull/92891

turbopack/crates/turbopack-ecmascript/src/analyzer/imports.rs
@@ -1223,17 +1223,26 @@ impl Visit for Analyzer<'_> {
     }
 
     fn visit_member_expr(&mut self, node: &MemberExpr) {
-        if let MemberProp::Ident(..) | MemberProp::PrivateName(..) = &node.prop
-            && node.obj.is_ident()
+        if matches!(
+            &node.prop,
+            MemberProp::Ident(..) | MemberProp::PrivateName(..)
+        ) && let Expr::Ident(ident) = &*node.obj
         {
-            // Skip traversing if obj is a Expr::Ident, so that it doesn't get added to
+            // Intentionally skipping over visit_expr(node.obj) here so that it doesn't get added to
             // full_star_imports below in visit_expr.
+            ident.visit_with(self);
+        } else {
+            node.visit_children_with(self);
+        }
+    }
 
-            // TODO this currently doesn't properly mark the import in self.program_decl_usage, see
-            // todo in
-            // turbopack/crates/turbopack-tests/tests/execution/turbopack/remove-unused-imports/
-            // import-star/input/index.js
-            return;
+    fn visit_expr(&mut self, node: &Expr) {
+        // Careful about adding anything here, visit_member_expr might skip over this method for
+        // some Expr::Ident-s.
+        if let Expr::Ident(i) = node
+            && let Some(module_path) = self.namespace_imports_to_specifier.get(&i.to_id())
+        {
+            self.data.full_star_imports.insert(module_path.clone());
         }
         node.visit_children_with(self);
     }
@@ -1254,15 +1263,6 @@ impl Visit for Analyzer<'_> {
             if let Some(module_path) = self.namespace_imports_to_specifier.get(&i.to_id()) {
                 self.data.full_star_imports.insert(module_path.clone());
             }
-        }
-        node.visit_children_with(self);
-    }
-
-    fn visit_expr(&mut self, node: &Expr) {
-        if let Expr::Ident(i) = node
-            && let Some(module_path) = self.namespace_imports_to_specifier.get(&i.to_id())
-        {
-            self.data.full_star_imports.insert(module_path.clone());
         }
         node.visit_children_with(self);
     }

turbopack/crates/turbopack-tests/tests/execution/turbopack/remove-unused-imports/member-call/input/a.js
@@ -1,0 +1,9 @@
+import { b } from './b.js'
+
+export function a() {
+  return sub.call()
+}
+
+function sub() {
+  return b
+}

turbopack/crates/turbopack-tests/tests/execution/turbopack/remove-unused-imports/member-call/input/b.js
@@ -1,0 +1,1 @@
+export const b = 123

turbopack/crates/turbopack-tests/tests/execution/turbopack/remove-unused-imports/member-call/input/index.js
@@ -1,0 +1,5 @@
+import { a } from './a.js'
+
+it('should work', () => {
+  expect(a()).toBe(123)
+})