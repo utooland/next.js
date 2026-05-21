97a2f0ade3c506127cbb81bdac826d427131e315
郁蒸十四 <55919198+yuzheng14@users.noreply.github.com>
2026-05-21 11:25:48 +0800

    Revert "TURBOPACK: Fix subpath imports resolving to dependencies (#150)"
    This reverts commit e3b7a696dc74310051ca7a21fc9587464e669dc8.

--- turbopack/crates/turbopack-tests/tests/snapshot/imports/subpath-imports/input/index.js
+++ turbopack/crates/turbopack-tests/tests/snapshot/imports/subpath-imports/input/index.js
@@ -1,8 +1,7 @@
 import foo from '#foo'
 import dep from '#dep'
-import depInNm from '#dep-in-nm'
 import pattern from '#pattern/pat.js'
 import conditionalImport from '#conditional'
 const conditionalRequire = require('#conditional')
 
-console.log(foo, dep, depInNm, pattern, conditionalImport, conditionalRequire)
+console.log(foo, dep, pattern, conditionalImport, conditionalRequire)
