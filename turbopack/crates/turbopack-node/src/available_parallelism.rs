d6cce4c685a524dd1f92194cd8a0bb0033e0665e
xusd320 <xusd320@gmail.com>
2026-06-06 18:16:53 +0800

    fix(pack): diable loader concurrency on wasm

--- turbopack/crates/turbopack-node/src/available_parallelism.rs
+++ turbopack/crates/turbopack-node/src/available_parallelism.rs
@@ -1,15 +1,17 @@
 #[cfg(all(target_family = "wasm", target_os = "unknown"))]
 pub fn available_parallelism() -> usize {
-    use wasm_bindgen::{JsCast, prelude::*};
-    let nav = js_sys::Reflect::get(&js_sys::global(), &JsValue::from_str("navigator"));
-    if let Ok(nav) = nav {
-        let hc = js_sys::Reflect::get(&nav, &JsValue::from_str("hardwareConcurrency"));
-        if let Ok(hc) = hc {
-            if let Some(n) = hc.as_f64() {
-                return n as usize;
-            }
-        }
-    }
+    // use wasm_bindgen::{JsCast, prelude::*};
+    // let nav = js_sys::Reflect::get(&js_sys::global(), &JsValue::from_str("navigator"));
+    // if let Ok(nav) = nav {
+    //     let hc = js_sys::Reflect::get(&nav, &JsValue::from_str("hardwareConcurrency"));
+    //     if let Ok(hc) = hc {
+    //         if let Some(n) = hc.as_f64() {
+    //             return n as usize;
+    //         }
+    //     }
+    // }
+
+    //TODO: Enable concurrency may cause OPFS error, which can't resolve files under .turbopack/
     1
 }
 
